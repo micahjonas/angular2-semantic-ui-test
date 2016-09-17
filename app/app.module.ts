3ee9c47706f25e637360e41fb87a6f9ac4916949
Micha Schwendener <micha.schwendener@gmail.com>
2016-09-17 15:42:18 +0200

    Initial setup for angular2 from the official tutorial

---
+++ app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { routing } from './app.routing';
+
+import { AppComponent }   from './app.component';
+import { HeroesComponent }   from './heroes.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService }         from './hero.service';
+
+@NgModule({
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    routing
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
---
+++ app/heroes.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: 'app/heroes.component.html',
+  styleUrls:  ['app/heroes.component.css']
+})
+
+
+
+
+export class HeroesComponent implements OnInit {
+  title = 'Tour of Heros';
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
